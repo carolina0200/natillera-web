30f015e05df969fa5660e6da26236f029fa28d0a
Carolina Giraldo <caroliina20003@gmail.com>
2019-06-01 15:30:28 -0500

    Primer commit

---
+++ src/main/java/com/curso/natillera/natillera/NatilleraApplication.java
@@ -0,0 +1,13 @@
+package com.curso.natillera.natillera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatilleraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatilleraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/natillera/natillera/NatilleraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.natillera.natillera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatilleraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
